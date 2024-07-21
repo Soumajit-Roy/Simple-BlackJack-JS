0a76d541d2cba6fd3bf1a0117c2cb3bc3f741036
Soumajit Roy <itssoumajit.alt@gmail.com>
2024-07-21 22:10:28 +0530

    halfway

---
+++ index.js
@@ -0,0 +1,36 @@
+let firstCard = 10
+let secondCard = 1
+let cards = [firstCard, secondCard]
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardEl = document.getElementById("card-el")
+
+function startGame(){
+    renderGame()
+}
+
+function renderGame() {
+if (sum <= 20) {
+    message = "Do you want to draw a new card? 🙂"
+} else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack! 🥳"
+    hasBlackJack = true
+} else {
+    message = "You're out of the game! 😭"
+    isAlive = false
+}
+cardEl.textContent = "Cards : " + cards[0] + "," + cards[1]
+sumEl.textContent = "Sum : " + sum
+messageEl.textContent = message
+}
+
+function newCard() {
+    let card = 5
+    sum += card
+    cards.push(card)
+    renderGame()
+}
